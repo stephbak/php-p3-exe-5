8d8477530e552aed373c8bce7635c65cac932165
stephbak <stephane.bakum@sfr.fr>
2019-01-07 10:20:27 +0100

    p3 exe5

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<link rel="stylesheet" href="style.css" />
+<head>
+  <meta charset="utf-8" />
+  <title>php partie3</title>
+</head>
+<body>
+  <?php for($number = 1; $number <= 15; $number++){ ?>
+    <p>On y arrive presque!</p>
+  <?php } ?>
+</body>
+</html>
